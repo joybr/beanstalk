9ad63106b8b0ec90f0bf26fb4a563457eb3567a4
cocoboram <brkim.work@gmail.com>
2018-07-24 18:18:17 +0900

    python test

---
+++ turle1.py
@@ -0,0 +1,15 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.forward(1)
+
+turtle.circle(100)
+turtle.right(90)
+turtle.circle(100)
+turtle.right(90)
+turtle.circle(100)
+turtle.right(90)
+turtle.circle(100)
+
+
+
